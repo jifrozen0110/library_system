aa0e6e4aadd9ade4166b34a894fcee38754b2425
jifrozen0110 <answldjs1836@gmail.com>
2025-02-09 00:20:03 +0900

    feat : swagger 설정

---
+++ src/main/java/com/book/librarysystem/config/ApiDocsConfig.java
@@ -0,0 +1,22 @@
+package com.book.librarysystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class ApiDocsConfig {
+	@Bean
+	public OpenAPI openAPI(){
+		Info info = new Info()
+			.title("Library System API")
+			.version("v.0.0.1")
+			.description("도서 관리 시스템 API 명세서입니다.");
+		return new OpenAPI().components(new Components()).info(info);
+
+	}
+}
