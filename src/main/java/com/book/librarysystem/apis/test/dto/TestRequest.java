a723f7a85caae4f6e03480fe63286e73d6aa06bd
jifrozen0110 <answldjs1836@gmail.com>
2025-02-09 00:33:10 +0900

    feat : 테스트 데이터 schema 관련 문서화 추가

--- src/main/java/com/book/librarysystem/apis/test/dto/TestRequest.java
+++ src/main/java/com/book/librarysystem/apis/test/dto/TestRequest.java
@@ -1,10 +1,13 @@
 package com.book.librarysystem.apis.test.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "테스트 요청 데이터")
 public class TestRequest {
+	@Schema(description = "테스트 데이터")
 	private String data;
 }
