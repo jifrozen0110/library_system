9525b34123fd1fccbf9140076d265cedd4fb0855
jifrozen0110 <answldjs1836@gmail.com>
2025-02-08 23:24:42 +0900

    feat : 기본 도메인 설계

--- src/main/java/com/book/librarysystem/LibrarySystemApplication.java
+++ src/main/java/com/book/librarysystem/LibrarySystemApplication.java
@@ -2,8 +2,10 @@ package com.book.librarysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
