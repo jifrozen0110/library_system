6af4c45e53e6560ad18fcdb708f9205cb6919196
jifrozen0110 <answldjs1836@gmail.com>
2025-02-10 16:34:59 +0900

    feat : 테스트 DB 추가

--- src/test/java/com/book/librarysystem/LibrarySystemApplicationTests.java
+++ src/test/java/com/book/librarysystem/LibrarySystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.book.librarysystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LibrarySystemApplicationTests {
 
 	@Test
