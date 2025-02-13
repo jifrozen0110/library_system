30361223307e9dfdbbd1566842745c60516dcd1b
jifrozen0110 <answldjs1836@gmail.com>
2025-02-13 18:01:48 +0900

    style : reformat code

--- src/main/java/com/book/librarysystem/domains/user/exception/UserEmailInvalidException.java
+++ src/main/java/com/book/librarysystem/domains/user/exception/UserEmailInvalidException.java
@@ -1,6 +1,6 @@
 package com.book.librarysystem.domains.user.exception;
 
-public class UserEmailInvalidException extends RuntimeException{
+public class UserEmailInvalidException extends RuntimeException {
 	public UserEmailInvalidException(String value) {
 		super("이메일 형식이 올바르지 않습니다: " + value);
 	}
--- src/main/java/com/book/librarysystem/domains/user/exception/UserNameLengthException.java
+++ src/main/java/com/book/librarysystem/domains/user/exception/UserNameLengthException.java
@@ -1,6 +1,6 @@
 package com.book.librarysystem.domains.user.exception;
 
-public class UserNameLengthException extends RuntimeException{
+public class UserNameLengthException extends RuntimeException {
 	public UserNameLengthException(int allowedLength, int actualLength) {
 		super(String.format("멤버 이름의 길이가 최대 이름 길이를 초과했습니다. - 요청 정보 { 최대 길이 : %d, 입력한 길이 : %d }",
 			allowedLength,
