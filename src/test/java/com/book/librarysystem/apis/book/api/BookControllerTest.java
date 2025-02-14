86fbc74278a6885171d56c930a1a68106f1d142a
jifrozen0110 <answldjs1836@gmail.com>
2025-02-14 22:12:15 +0900

    fix : import 정리

--- src/test/java/com/book/librarysystem/apis/book/api/BookControllerTest.java
+++ src/test/java/com/book/librarysystem/apis/book/api/BookControllerTest.java
@@ -1,6 +1,5 @@
 package com.book.librarysystem.apis.book.api;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
