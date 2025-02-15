f732cd4147c6e28035e13f6f2373e392d853e1bb
jifrozen0110 <answldjs1836@gmail.com>
2025-02-15 15:15:41 +0900

    refactor : config 파일 패키지 경로 변경

--- src/main/java/com/book/librarysystem/globals/config/ApiDocsConfig.java
+++ src/main/java/com/book/librarysystem/globals/config/ApiDocsConfig.java
@@ -1,4 +1,4 @@
-package com.book.librarysystem.config;
+package com.book.librarysystem.globals.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
