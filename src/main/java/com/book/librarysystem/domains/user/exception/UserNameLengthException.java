44f815019ffb171eba50f75d5ed7bdfa27c4dd65
jifrozen0110 <answldjs1836@gmail.com>
2025-02-17 21:32:00 +0900

    fix : Exception 상속 수정

--- src/main/java/com/book/librarysystem/domains/user/exception/UserNameLengthException.java
+++ src/main/java/com/book/librarysystem/domains/user/exception/UserNameLengthException.java
@@ -1,6 +1,8 @@
 package com.book.librarysystem.domains.user.exception;
 
-public class UserNameLengthException extends RuntimeException {
+import com.book.librarysystem.globals.exception.CustomBadRequestException;
+
+public class UserNameLengthException extends CustomBadRequestException {
 	public UserNameLengthException(int allowedLength, int actualLength) {
 		super(String.format("멤버 이름의 길이가 최대 이름 길이를 초과했습니다. - 요청 정보 { 최대 길이 : %d, 입력한 길이 : %d }",
 			allowedLength,
