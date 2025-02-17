1f2f9dc521dd98f1f682884a808c605c29ac53d3
jifrozen0110 <answldjs1836@gmail.com>
2025-02-17 21:32:00 +0900

    fix : Exception 상속 수정

--- src/main/java/com/book/librarysystem/domains/user/exception/UserNotFoundException.java
+++ src/main/java/com/book/librarysystem/domains/user/exception/UserNotFoundException.java
@@ -1,6 +1,8 @@
 package com.book.librarysystem.domains.user.exception;
 
-public class UserNotFoundException extends RuntimeException {
+import com.book.librarysystem.globals.exception.CustomNotFoundException;
+
+public class UserNotFoundException extends CustomNotFoundException {
 	public UserNotFoundException() {
 		super("사용자를 찾을 수 없습니다.");
 	}
